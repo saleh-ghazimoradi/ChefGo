90b4c3a0a0ed20699895fc568758c1a07a0061d2
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2024-09-24 23:38:49 +0330

    establish mongoDB connection

---
+++ utils/connection.go
@@ -0,0 +1,32 @@
+package utils
+
+import (
+	"context"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+	"time"
+)
+
+func ConnectMongoDB() (*mongo.Client, error) {
+	uri := "mongodb://admin:secret@localhost:27017"
+
+	clientOptions := options.Client().ApplyURI(uri)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		return nil, err
+	}
+
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	log.Println("Connected to MongoDB!")
+
+	return client, nil
+}
