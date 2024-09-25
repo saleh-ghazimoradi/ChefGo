2c9f0ba5b1033fe6f008b8f8c440895c7980feb6
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2024-09-25 17:20:54 +0330

    move the password and validation to the heart of service layer

--- internal/gateway/http/user.go
+++ internal/gateway/http/user.go
@@ -1,1 +1,32 @@
 package http
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/saleh-ghazimoradi/ChefGo/internal/service"
+)
+
+type userHandler struct {
+	userService service.User
+}
+
+func (u *userHandler) GetUser(c *fiber.Ctx) error {
+	return nil
+}
+
+func (u *userHandler) GetUsers(c *fiber.Ctx) error {
+	return nil
+}
+
+func (u *userHandler) SignUp(c *fiber.Ctx) error {
+	return nil
+}
+
+func (u *userHandler) Login(c *fiber.Ctx) error {
+	return nil
+}
+
+func NewUserHandler(userService service.User) *userHandler {
+	return &userHandler{
+		userService: userService,
+	}
+}
