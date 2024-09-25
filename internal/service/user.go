9ba62fe55bcdd1ef4e8607fa26bb32e751dbeb4d
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2024-09-25 17:22:38 +0330

    grab the password and validation from the application layer and put them in the service layer

--- internal/service/user.go
+++ internal/service/user.go
@@ -11,6 +11,8 @@ type User interface {
 	GetUsers(ctx context.Context) ([]service_models.User, error)
 	CreateUser(ctx context.Context, menu service_models.User) error
 	UpdateUser(ctx context.Context, id string) error
+	HashPassword(password string) (string, error)
+	VerifyPassword(hashedPassword, password string) (bool, error)
 }
 
 type userService struct {
@@ -31,6 +33,14 @@ func (s *userService) CreateUser(ctx context.Context, menu service_models.User) 
 
 func (s *userService) UpdateUser(ctx context.Context, id string) error {
 	return nil
+}
+
+func (s *userService) HashPassword(password string) (string, error) {
+	return "", nil
+}
+
+func (s *userService) VerifyPassword(hashedPassword, password string) (bool, error) {
+	return false, nil
 }
 
 func NewUserService(userRepo repository.User) User {
