b8ad91d675061c0f1fe382c851c99d05443740da
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2024-09-25 14:26:10 +0330

    add user service to the project

---
+++ internal/service/user.go
@@ -0,0 +1,38 @@
+package service
+
+import (
+	"context"
+	"github.com/saleh-ghazimoradi/ChefGo/internal/repository"
+	service_models "github.com/saleh-ghazimoradi/ChefGo/internal/service/service-models"
+)
+
+type User interface {
+	GetUser(ctx context.Context, id string) (service_models.User, error)
+	GetUsers(ctx context.Context) ([]service_models.User, error)
+	CreateUser(ctx context.Context, menu service_models.User) error
+	UpdateUser(ctx context.Context, id string) error
+}
+
+type userService struct {
+	userRepo repository.User
+}
+
+func (s *userService) GetUser(ctx context.Context, id string) (service_models.User, error) {
+	return service_models.User{}, nil
+}
+
+func (s *userService) GetUsers(ctx context.Context) ([]service_models.User, error) {
+	return nil, nil
+}
+
+func (s *userService) CreateUser(ctx context.Context, menu service_models.User) error {
+	return nil
+}
+
+func (s *userService) UpdateUser(ctx context.Context, id string) error {
+	return nil
+}
+
+func NewUserService(userRepo repository.User) User {
+	return &userService{userRepo: userRepo}
+}
